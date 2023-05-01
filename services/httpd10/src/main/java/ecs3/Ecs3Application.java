2b9fcd2166bcb7cc37673a8756824b39d0bb580b
philanimalandrews <65134755+philanimalandrews@users.noreply.github.com>
2023-05-01 13:26:41 +0100

    Attempt to get correct project structure

---
+++ services/httpd10/src/main/java/ecs3/Ecs3Application.java
@@ -0,0 +1,15 @@
+package ecs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecs3Application {
+
+    public static void main(String[] args) {
+        System.out.println(111222);
+
+        SpringApplication.run(Ecs3Application.class, args);
+    }
+
+}
